32b8bc7854d9c3fc9483632c6f4fa343941b33c6
bSharpd <lightsail@MacBook-Air-7.local>
2017-07-24 17:46:03 -0400

    Index html

---
+++ pikaMUhelp.js
@@ -0,0 +1,184 @@
+var pika = {};
+pika.bowser= {dtilt : 96, uthrowThun : "101%-167%", fairUsmash : "130ish",uthrow : "SV:215%", bthrow : "Edge:186%", bfjabconfirm : "yes", notes : ""};
+
+
+pika.dk= {dtilt : 94, uthrowThun : "104%-171", fairUsmash : "130ish",uthrow : "SV:223", bthrow : "Edge:182", bfjabconfirm : "yes", notes : ""};
+
+
+pika.dedede= {dtilt : 92, uthrowThun : "118%-186%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.charizard= {dtilt : 90, uthrowThun : "90%-150%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.ganon= {dtilt : 90, uthrowThun : "118%-160%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.bowserjr= {dtilt : 88, uthrowThun : "114%-159%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.samus= {dtilt : 87, uthrowThun : "98%-149%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.wario= {dtilt : 87, uthrowThun : "105%-157%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.ike= {dtilt : 87, uthrowThun : "115%-163%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.rob= {dtilt : 86, uthrowThun : "95%-153%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.yoshi= {dtilt : 86, uthrowThun : "93%-143%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.link= {dtilt : 86, uthrowThun : "110%-150%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.falcon= {dtilt : 86, uthrowThun : "121%-167%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.ryu= {dtilt : 85, uthrowThun : "112%-156%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.shulk= {dtilt : 85, uthrowThun : "105%-150%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.megaman= {dtilt : 85, uthrowThun : "116%-155%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.cloud= {dtilt : 84, uthrowThun : "110%-157%, Limit: 118%-167%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.lucario= {dtilt : 83, uthrowThun : "111%-160%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.mario= {dtilt : 83, uthrowThun : "103%-145%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.doctormario= {dtilt : 83, uthrowThun : "104%-142%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.corrin= {dtilt : 83, uthrowThun : "111%-153%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.luigi= {dtilt : 82, uthrowThun : "94%-132%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.villager= {dtilt : 82, uthrowThun : "95%-141%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.pit= {dtilt : 82, uthrowThun : "100%-144%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.darkpit= {dtilt : 82, uthrowThun : "100%-144%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.wiifittrainer= {dtilt : 82, uthrowThun : "91%-135%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.roy= {dtilt : 82, uthrowThun : "114%-150%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.robin= {dtilt : 82, uthrowThun : "103%-146%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.pacman= {dtilt : 82, uthrowThun : "97%-138%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.sonic= {dtilt : 81, uthrowThun : "100%-141%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.greninja= {dtilt : 81, uthrowThun : "98%-136%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.ness= {dtilt : 81, uthrowThun : "98%-131%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.lucas= {dtilt : 81, uthrowThun : "95%-135%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.toonlink= {dtilt : 81, uthrowThun : "93%-133%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.diddy= {dtilt : 81, uthrowThun : "105%-145%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.palutena= {dtilt : 80, uthrowThun : "93%-133%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.duckhunt= {dtilt : 80, uthrowThun : "101%-146%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.marth= {dtilt : 79, uthrowThun : "104%-145%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.lucina= {dtilt : 79, uthrowThun : "106%-145%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.peach= {dtilt : 78, uthrowThun : "96%-129%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.zelda= {dtilt : 77, uthrowThun : "90%-134%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.sheik= {dtilt : 77, uthrowThun : "99%-135%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.bayonetta= {dtilt : 77, uthrowThun : "115%-147%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.falco= {dtilt : 76, uthrowThun : "99%-135%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.littlemac= {dtilt : 76, uthrowThun : "104%-146%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.zerosuitsamus= {dtilt : 75, uthrowThun : "101%-144%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.metaknight= {dtilt : 75, uthrowThun : "98%-135%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.kirby= {dtilt : 74, uthrowThun : "84%-131%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.fox= {dtilt : 74, uthrowThun : "106%-133%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.pikachu= {dtilt : 74, uthrowThun : "90%-132%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.olimar= {dtilt : 74, uthrowThun : "87%-130%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.rosalina= {dtilt : 74, uthrowThun : "89%-129%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.MrGW= {dtilt : 73, uthrowThun : "74%-114%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+pika.mewtwo= {dtilt : 71, uthrowThun : "94%-134%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "yes", notes : ""};
+
+
+pika.jigglypuff= {dtilt : 69, uthrowThun : "77%-117%", fairUsmash : "",uthrow : "", bthrow : "", bfjabconfirm : "no", notes : ""};
+
+
+function makeButtons(){
+    for(var x in pika){
+		var button=document.createElement("button");
+		button.innerHTML=x;
+		document.body.appendChild(button);
+		//document.body.appendChild(document.createElement("br"));
+		button.onclick=function(event){
+			document.getElementById('dtilt').value=pika[event.target.innerHTML].dtilt;
+			document.getElementById('turnt').value=pika[event.target.innerHTML].uthrowThun;
+			document.getElementById('fairusmash').value=pika[event.target.innerHTML].fairUsmash;
+			document.getElementById('uthrow').value=pika[event.target.innerHTML].uthrow;
+			document.getElementById('bthrow').value=pika[event.target.innerHTML].bthrow;
+			document.getElementById('platjab').value=pika[event.target.innerHTML].bfjabconfirm;
+			document.getElementById('genmunotes').value=pika[event.target.innerHTML].notes;
+		};
+	}
+	}
+makeButtons();
