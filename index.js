150827169cc13526cf15e61a10ae45242ed2553b
bSharpd <lightsail@MacBook-Air-7.local>
2017-08-03 20:26:01 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 245930f6eb8d88a02798a57e6798f89e2b926584, reversing
    changes made to a60992e6887e5d851f7b9c75e73e75f17d717f8b.

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function makeButtons(){
 		var button=document.createElement("button");
 		button.innerHTML=x;
 		button.className="charbtn";
-		document.getElementById('btnlimit').appendChild(button);
+		document.getElementById('btndiv').appendChild(button);
 		button.onclick=function(event){
 			document.getElementById('dair').value=pika[event.target.innerHTML].dair;
 			document.getElementById('nair').value=pika[event.target.innerHTML].nair;
